84a92d6bc073c8a537ed248853d654e75157bf3e
zhaobo <tyounami@gmail.com>
2022-02-18 02:06:29 +0800

    FEAT: colorSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState, useMemo } from 'react'
 import ColorSelector from './components/color-selector';
 import Grid from './components/grid';
-import ModeSelector from './components/mode-selector';
 import './App.css'
 
 function App() {
-
+  const [colorType, setColorType] = useState(1);
   return (
     <div className="App">
-      <ModeSelector />
       <Grid />
-      <ColorSelector />
+      <ColorSelector
+        value={colorType}
+        onChange={setColorType}
+      />
     </div>
   )
 }
--- src/components/color-selector/index.jsx
+++ src/components/color-selector/index.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
-
-const ModeSelector = () => {
+import classnames from 'classnames';
+import styles from './index.module.less';
+const ModeSelector = ({ value, onChange }) => {
     return (
-        <h1>111</h1>
+        <ul className={styles.container}>
+            <li
+                className={classnames({
+                    [styles.gray]: true,
+                    [styles.selected]: value === 0,
+                })}
+                onClick={() => {
+                    onChange(0);
+                }}
+            >
+
+            </li>
+            <li
+                className={classnames({
+                    [styles.green]: true,
+                    [styles.selected]: value === 1,
+                })}
+                onClick={() => {
+                    onChange(1);
+                }}
+            >
+
+            </li>
+            <li
+                className={classnames({
+                    [styles.yellow]: true,
+                    [styles.selected]: value === 0.5,
+                })}
+                onClick={() => {
+                    onChange(0.5);
+                }}
+            ></li>
+        </ul>
     )
 };
 
